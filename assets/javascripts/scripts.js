3b59f93a401cba3dda3e21743e652e5078978486
dev-seahouse <xinkenan.11@hotmail.com>
2013-11-20 18:33:46 +0800

    added !default to colors

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -15,13 +15,17 @@ jQuery(document).ready(function() {
             });
             return false;
         }
-        if(password == '') {
+        else if(password == '') {
             $(this).find('.error').fadeOut('fast', function(){
                 $(this).css('top', '143px');
             });
             $(this).find('.error').fadeIn('fast', function(){
                 $(this).parent().find('.password').focus();
             });
+            return false;
+        }
+        else {
+            window.location.href="test.html";
             return false;
         }
     });
