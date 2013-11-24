4f73b72e2d76192ad0337ee1f3bbf9794522989c
dev-seahouse <xinkenan.11@hotmail.com>
2013-11-24 12:03:13 +0800

    addresspicker changed to bootstrap

--- assets/javascripts/plugins/addresspicker/jquery.ui.addresspicker.js
+++ assets/javascripts/plugins/addresspicker/jquery.ui.addresspicker.js
@@ -41,7 +41,7 @@
             type: false
 
         },
-        autocomplete: 'bootsrap' // could be autocomplete: "bootstrap" to use bootstrap typeahead autocomplete instead of jQueryUI
+        autocomplete: 'bootstrap' // could be autocomplete: "bootstrap" to use bootstrap typeahead autocomplete instead of jQueryUI
     },
 
     marker: function() {
