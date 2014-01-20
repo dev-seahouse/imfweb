d0e0b67cb2bbb13b608febb65ad61ac305807668
dev-seahouse <xinkenan.11@hotmail.com>
2014-01-21 02:32:14 +0800

    added modified files

---
+++ classes/login.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* login class handles login and logout process
+*/
+
+class login
+{
+	/**
+	 * @var object declare database connection
+	 * 
+	 */
+	private $db_connection=null;
+	/**
+	 * @var array Collection of error messages
+	 */
+	public $errors=array();
+	/**
+	 * @var array Collection of succesful message
+	 */
+	public $messages=array();
+	
+
+
+
+}
