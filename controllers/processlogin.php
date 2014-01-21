46f1e09fe03f0168d1d5cbca6baf576420d1b616
dev-seahouse <xinkenan.11@hotmail.com>
2014-01-21 14:45:23 +0800

    added controller, moved index.html to controller.php

---
+++ controllers/processlogin.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("libraries/password_compatibility_library_php");
+    require_once("config/db.php");
+    require_once("classes/Login.php")
+
+    $login=new Login();
+    if ($login->isUserLoggedIn()==true){
+    	header("Location: user_profile.html");
+		exit;
+
+    }
+ ?>
