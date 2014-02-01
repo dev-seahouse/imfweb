ef5b3591922ca3f664e3303363b66834e74094e4
dev-seahouse <xinkenan.11@hotmail.com>
2014-02-02 03:00:42 +0800

    a number of changes,start ajax display scope

--- controllers/processPostJob.php
+++ controllers/processPostJob.php
@@ -1,29 +1,28 @@
 <?php
 require_once("libraries/utilities.php");
 require_once("classes/JobCategory.php");
-class Postjob
-{
-    
+/*========================================================================
+  =            All postjob presentational controllers here            =
+  ========================================================================*/
+
     // public $errors=array();
     // public $messages=array();
 
-    public function __construct(){
-        check_auth("index.php");
-    }
-    public function populate_jobcategory(){
+     check_auth("index.php");
+     
+     
+     function populate_jobcategory(){
         $jobCategory=new JobCategory();
         $categories=$jobCategory->get_all_categories();
         foreach ($categories as $row) {
             echo "<option value='".$row["CategoryID"]."'>".$row["CategoryName"]."</option>\n";
         }
         //var_dump($categories);
-        
-
     }
 
-
-}
-
+    function populate_jobscope(){
+        
+    }
 ?>
 
 
